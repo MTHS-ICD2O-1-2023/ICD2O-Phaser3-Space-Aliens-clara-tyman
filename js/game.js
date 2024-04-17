39464cc863569dbf37861b4b2026b168658a874c
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-04-17 18:39:45 +0000

    colour!!!!

--- js/game.js
+++ js/game.js
@@ -1,14 +1,9 @@
 /* global Phaser */
-
-/**
- * Start Phaser Game.
- */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  // set background colour
   backgroundColor: 0x5f6e7a
 }
-const game = new Phaser.game(config)
+const game = new Phaser.Game(config)
 console.log(game)
