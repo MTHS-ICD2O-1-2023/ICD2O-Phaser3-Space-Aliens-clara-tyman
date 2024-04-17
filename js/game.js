7b7bf06dd4c5e21e039f7d6652aed436e0e8f01f
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-04-17 18:04:02 +0000

    js file added and code

---
+++ js/game.js
@@ -0,0 +1,17 @@
+/*global Phaser */
+
+// Copyright (c) 2024 Clara Tyman All rights reserved
+//
+// Created by: Clara Tyman
+// Created on: April 2024
+// This file contains the JS functions for index.html
+
+const cpmfig = {
+  type: Phaser.AUTO,
+  width: 1920,
+  height: 1880,
+  backgroundColor: 0x5f6e7a
+  }
+
+const game = new Phaser.Game(config)
+console.log(game)
